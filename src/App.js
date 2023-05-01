e67cf4eb629eae61d230e131556bae351ba81644
Bruno Weiler Foletto <brunoweiller@hotmail.com>
2023-05-01 20:21:12 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
-    <div className="bg-purple-500">
-      <h1 className="text-xl">Hello world!</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar></Navbar>
+        <main className="container mx-auto pb-12">
+          <Routes>
+            <Route path="/" element={<Home></Home>}></Route>
+            <Route path="/about" element={<About></About>}></Route>
+            <Route path="/notfound" element={<NotFound></NotFound>}></Route>
+            <Route path="/*" element={<NotFound></NotFound>}></Route>
+          </Routes>
+        </main>
+        <Footer></Footer>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import UserResults from "../components/users/UserResults";
+
+function Home() {
+  return (
+    <div>
+      {/* SEARCH COMPONENT */}
+      <UserResults></UserResults>
+    </div>
+  );
+}
+
+export default Home;
